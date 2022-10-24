f2db6fd8055f557b34fd5debb99c1966c7926374
Marcel Stadelmann <m@marceli.to>
2022-10-24 10:12:09 +0200

    updat

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,7 +19,6 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    // After...
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
