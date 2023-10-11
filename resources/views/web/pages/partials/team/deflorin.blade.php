89fe16de2a3dcea853bbe892273583445f550f31
Marcel Stadelmann <m@marceli.to>
2023-10-11 15:11:53 +0200

    wip

---
+++ resources/views/web/pages/partials/team/deflorin.blade.php
@@ -0,0 +1,46 @@
+<section class="content-team">
+    <div>
+      <article class="text-media team">
+        <figure class="text-media__image">
+          <img src="/assets/media/team/mmt_team_sue_deflorin_martin.jpg" width="1076" height="720" class="is-responsive" alt="Suê Deflorin Martin">
+        </figure>
+        <div class="text-media__body">
+            <h2>Suê Deflorin Martin</h2>
+            <p>
+              Eidg. dipl. Bauleiterin Hochbau<br>Projekt- und Bauleiterin
+            </p>
+            <p>
+              T 052 235 08 85<br>
+              M 079 675 30 40<br>
+              <a href="mailto:sue.deflorin@mmt-ag.ch">sue.deflorin@mmt-ag.ch</a>
+            </p>
+            <div class="btn-wrapper">
+              <a href="javascript:;" class="btn-cv js-btn-cv" data-target="deflorin"></a>
+            </div>
+        </div>
+      </article>
+      <div class="team-cv" data-cv="deflorin" style="display:none">
+        <h3>Lebenslauf</h3>
+        <h4>Berufliche Tätigkeit</h4>
+        <table class="cv">
+          <tr><td>2023&thinsp;–&thinsp;Heute</td><td>Projekt- und Bauleiterin bei MMT AG Bauleiter und Architekten, Zürich</td></tr>
+          <tr><td>2020&thinsp;–&thinsp;2023</td><td>Projekt- und Bauleiterin bei Konstruo AG, Winterthur</td></tr>
+          <tr><td>2009&thinsp;–&thinsp;2020</td><td>Projekt- und Bauleiterin bei MMT AG Bauleiter und Architekten, Winterthur</td></tr>
+          <tr><td>2004&thinsp;–&thinsp;2009</td><td>Bauleiterin bei Mooser Treichler Bauleiter, Brüttisellen</td></tr>
+          <tr><td>1997&thinsp;–&thinsp;2004</td><td>Zeichnerin und Bauleiterin bei René Olivier AG Ingenieure + Planer, Dietlikon</td></tr>
+          <tr><td>1996&thinsp;–&thinsp;1997</td><td>Hochbauzeichnerin bei Landolt + Haller Architekten AG, Zürich</td></tr>
+          <tr><td>1992&thinsp;–&thinsp;1996</td><td>Lehre als Hochbauzeichnerin bei Landolt + Haller Architekten AG, Zürich</td></tr>
+          <tr><td>1976</td><td>geboren in Baden</td></tr>
+        </table>
+        <h4>Berufsbegleitende Weiterbildungen</h4>
+        <table class="cv">
+          <tr><td>2004</td><td>Diplom eidg. dipl. Bauleiterin Hochbau</td></tr>
+          <tr><td>2002&thinsp;–&thinsp;2004</td><td>Vorbereitungskurs Bauleiterin Hochbau GBW, Wetzikon</td></tr>
+          <tr><td>1999&thinsp;–&thinsp;2000</td><td>Hospitantin für Architekturgeschichte, Abend-Technikum, Zürich</td></tr>
+          <tr><td>1998</td><td>Diplom für Kaufmännische Sachbearbeiterin KS Kaderschule, Zürich</td></tr>
+          <tr><td>1997&thinsp;–&thinsp;1998</td><td>Diplom-Handelskurs KS Kaderschule, Zürich</td></tr>
+          <tr><td>1992&thinsp;–&thinsp;1996</td><td>Technische Berufsmittelschule GBW, Wetzikon</td></tr>
+        </table>
+      </div>
+    </div>
+  </section>
