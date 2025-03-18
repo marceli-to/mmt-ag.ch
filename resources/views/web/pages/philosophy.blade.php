21ce0e9e4c3e1280efc171b1fd6e9fd3e98a4a77
Marcel Stadelmann <m@marceli.to>
2025-03-18 08:41:43 +0100

    wip

--- resources/views/web/pages/philosophy.blade.php
+++ resources/views/web/pages/philosophy.blade.php
@@ -15,12 +15,12 @@
       <p>Unsere Spezialität sind architektonisch und technisch anspruchsvolle Hochbauten. Wir sorgen dafür, dass Ihr Bauprojekt nach Ihren Vorstellungen realisiert wird. Dabei garantieren wir in allen Bereichen Transparenz und geben Ihnen die Sicherheit, dass Kosten, Termine und Qualität eingehalten werden.</p>
       <p>Mit unseren Kundinnen und Kunden teilen wir die Begeisterung für gute Architektur; wir verstehen es, diese konkret umzusetzen. Die Sprache unserer Projektpartner verstehen wir genauso, wie die Sprache auf der Baustelle. Erfolgreich ergänzen wir mit unserer Fachkenntnis Teams beider Seiten.</p>
       <p>
-        <a href="/assets/downloads/MMT0025_Jubilaeum_Plakat.pdf" target="_blank" title="Download MMT 25 Jahre Plakat">
-          <img src="/assets/media/MMT0025_Jubilaeum_Plakat.jpg" height="1414" width="1000" class="half-width">
+        <a href="/assets/downloads/MMT0038_Broschuere.pdf" target="_blank" title="Download MMT-Broschüre">
+          <img src="/assets/media/MMT0038_Broschuere.jpg" height="1414" width="1000" class="half-width">
         </a>
       </p>
       <p>
-        <a href="/assets/downloads/MMT0025_Jubilaeum_Plakat.pdf" target="_blank" title="Download MMT 25 Jahre Plakat" class="anchor-chevron">MMT-Plakat</a><br>
+        <a href="/assets/downloads/MMT0038_Broschuere.pdf" target="_blank" title="Download MMT-Broschüre" class="anchor-chevron">MMT-Broschüre</a><br>
       </p>
     </article>
   </div>
